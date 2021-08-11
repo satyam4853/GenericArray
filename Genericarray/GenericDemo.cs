735403efb53b02994c09c43a0738fad20ca64bc1
Your Name <you@example.com>
2021-08-11 14:23:29 +0530

    adding generic array problem

---
+++ Genericarray/GenericDemo.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Genericarray
+{
+    public class GenericType<T>
+    {
+        private T[] inputArray;
+        public GenericType(T[] inputArray)
+        {
+            this.inputArray = inputArray;
+        }
+        public void Toprint()
+        {
+            foreach (var element in inputArray)
+            {
+                Console.WriteLine(element);
+            }
+            Console.WriteLine("-------------------------------------");
+
+        }
+    }
+}
---
+++ Genericarray/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Genericarray
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           int[] intArray = { 1, 2, 3, 4, 5, 6 };
+            double[] doubleArray = { 1.1, 1.2, 1.3, 1.41, 1.6 };
+            char[] charArray = { 'H', 'T', 'D', 'R' };
+            //Program.ToPrint<int>(intArray);
+            //Program.ToPrint<double>(doubleArray);
+            //Program.ToPrint<char>(charArray);
+            new GenericType<int>(intArray).Toprint();
+            new GenericType<double>(doubleArray).Toprint();
+            new GenericType<char>(charArray).Toprint();
+        }
+
+
+        //public static void ToPrint(double[] inputArray)
+        //{
+        //    foreach (double element in inputArray)
+        //    {
+        //        Console.WriteLine(element);
+        //    }
+        //    Console.WriteLine("-------------------------------");
+        //}
+        //public static void ToPrint<T>(T[] inputArray)
+        //{
+        //    foreach (var element in inputArray)
+        //    {
+        //        Console.WriteLine(element);
+        //    }
+        //    Console.WriteLine("-------------------------------------");
+        //}
+    }
+
+
+}
+
